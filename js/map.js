92f2078d0c72a55d41556f277bdec247a61dff0b
Shubin Alexander <shubin8@mail.ru>
2018-12-10 09:48:27 +0300

    на проверку

--- js/map.js
+++ js/map.js
@@ -26,7 +26,7 @@ var OFFER_TYPES = {
   bungalo: 'Бунгало'
 };
 var FLAT_PRICE = {
-  min: 1000,
+  min: 0,
   max: 1000000
 };
 var FLAT_ROOMS = {
@@ -57,6 +57,10 @@ var templatePin = document.querySelector('#pin').content.querySelector('.map__pi
 var templateCard = document.querySelector('#card').content.querySelector('.map__card');
 var roomNumber = document.querySelector('#room_number');
 var capacity = document.querySelector('#capacity');
+var timeIn = document.querySelector('#timein');
+var timeOut = document.querySelector('#timeout');
+var formType = document.querySelector('#type');
+var formPrice = document.querySelector('#price');
 
 // Массив со всеми объявлениями
 var advertArray = [];
@@ -250,16 +254,97 @@ var roomNumberСhangeHandler = function (connect) {
     var capacityInt = parseInt(capacity.value, 10);
     var roomInt = parseInt(roomNumber.value, 10);
 
-    if (capacityInt > roomInt && capacityInt > 0) {
+    if (capacityInt === roomInt && capacityInt > 0) {
       connect.setCustomValidity('Выберите соответсвующие значение (количество гостей не может превышать количество комнат)');
-    } else if (roomInt === 100 && capacityInt > 0) {
+    } else if (roomInt === 100 && capacityInt < 0) {
       connect.setCustomValidity('100 комнат не для гостей');
     } else if (roomInt !== 100 && capacityInt === 0) {
       connect.setCustomValidity('Выберите количество гостей');
     }
   });
 };
+
 roomNumberСhangeHandler(roomNumber);
 roomNumberСhangeHandler(capacity);
 
+roomNumber.addEventListener('change', function () {
+  capacity.selectedIndex = roomNumber.selectedIndex;
+});
 
+capacity.addEventListener('change', function () {
+  roomNumber.selectedIndex = capacity.selectedIndex;
+});
+
+// установка соответствия времени заезда
+timeIn.addEventListener('change', function () {
+  timeOut.selectedIndex = timeIn.selectedIndex;
+});
+
+timeOut.addEventListener('change', function () {
+  timeIn.selectedIndex = timeOut.selectedIndex;
+});
+
+// установка соответствия названия жилища и цены
+var compareFlatPrice = function () {
+  if (formType.value === 'bungalo') {
+    formPrice.min = 0;
+    formPrice.placeholder = 0;
+  } else if (formType.value === 'flat') {
+    formPrice.min = 1000;
+    formPrice.placeholder = 1000;
+  } else if (formType.value === 'house') {
+    formPrice.min = 5000;
+    formPrice.placeholder = 5000;
+  } else if (formType.value === 'palace') {
+    formPrice.min = 10000;
+    formPrice.placeholder = 10000;
+  }
+};
+
+var setValidation = function () {
+  formType.addEventListener('input', compareFlatPrice);
+};
+setValidation();
+
+
+// цикл Drag-and-drop для маркера
+/*
+var mapPin = document.querySelector('.map__pin--main');
+
+mapPin.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+
+  console.log(evt); I;
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
+
+var dialogHandle = setup.querySelector('.setup-user-pic');
+
+dialogHandle.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+});
+
+
+timeInSelect.addEventListener('change', function () {
+  timeOutSelect.selectedIndex = timeInSelect.selectedIndex;
+});
+
+timeOutSelect.addEventListener('change', function () {
+  timeInSelect.selectedIndex = timeOutSelect.selectedIndex;
+});
+
+var setDisabled = function (element) {
+  element.setAttribute('disabled', true);
+};
+
+var removeDisabled = function (element) {
+  element.removeAttribute('disabled');
+}
+*/
